24a599789f9ac5b1c4f33f4bc964fe31646f1570
Sanjeev Kumar <89408233+shelleycoop@users.noreply.github.com>
2024-07-06 13:36:44 +0530

    some mods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 
-const orm  = MikroORM.init();
+const main = async() => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'lireddit',
+        type: "postgresql",
+        debug: !__prod__,
+    });
 
-console.log('Hello There')
+    const post = orm.em.create(Post, {title: 'my first post'});
+    await orm.em.persistAndFlush(post);
+    console.log('--------sql 2---------')
+
+}
+
+main()
